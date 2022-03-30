73a9ad4cceaf5646ddc9c67b87f7547d25adca81
SavaIv <tie.mail@abv.bg>
2022-03-30 18:04:43 +0300

    Add Decimnal, Double and DateTime providers

---
+++ LawOffice.Core/Constants/FormatingConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace LawOffice.Core.Constants
+{
+    public class FormatingConstant
+    {
+        public const string NormalDateFormat = "dd.MM.yyyy";
+    }
+}
--- LawOffice/Program.cs
+++ LawOffice/Program.cs
@@ -1,4 +1,6 @@
+using LawOffice.Core.Constants;
 using LawOffice.Data;
+using LawOffice.ModelBinders;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,13 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllersWithViews()
+    .AddMvcOptions(options =>
+    {
+        options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+        options.ModelBinderProviders.Insert(1, new DoubleModelBinderProvider());
+        options.ModelBinderProviders.Insert(2, new DateTimeModelBinderProvider(FormatingConstant.NormalDateFormat));
+    });
 
 var app = builder.Build();
 
